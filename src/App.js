cba9a487b3374029a2c33de58d05b881e0c2a3f7
Zeeshan Haleem <zeeshanhaleem142@gmail.com>
2024-11-06 22:55:12 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React by Engr Zeeshan
+          React by Engr Zeeshan from Canada
         </a>
       </header>
     </div>
